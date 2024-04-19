6d1a62a4d992b7a78f1a75b49aa5b53307ec1b63
tddudwns1 <tjddudwns1@naver.com>
2024-04-20 00:59:49 +0900

    [BE][Feat] BaseTime 객체 생성
    #S10P31D104-76 #done
    #S10P31D104-77 #done
    #S10P31D104-78 #done

--- backend/src/main/java/com/todayeat/backend/BackendApplication.java
+++ backend/src/main/java/com/todayeat/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package com.todayeat.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class BackendApplication {
 
---
+++ backend/src/main/java/com/todayeat/backend/_common/entity/BaseTime.java
@@ -0,0 +1,37 @@
+package com.todayeat.backend._common.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseTime {
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = true)
+    private LocalDateTime deletedAt;
+
+    public String getCreatedAt() {
+        return createdAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+}
