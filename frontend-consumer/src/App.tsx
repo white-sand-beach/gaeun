08a6cfbcb22853dd42c6d388d561e20266202839
Anhyunsung <gustjd980112@yu.ac.kr>
2024-04-22 16:48:00 +0900

    [FE][Feat] @tanstack/react-query 설정

--- frontend-consumer/src/App.tsx
+++ frontend-consumer/src/App.tsx
@@ -1,8 +1,17 @@
+import React from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
-    <div className="text-3xl text-orange-500 font-extrabold">
-      <h1>Hello, world!</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <div className="text-3xl text-orange-500 font-extrabold">
+        <h1>Hello, world!</h1>
+      </div>
+    </QueryClientProvider>
   );
 };
 
