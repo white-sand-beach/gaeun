25a9f862d9d8d2c398dfd6cef7658fe07205ee07
parkjonghyuk <occultism22@naver.com>
2024-04-24 14:09:26 +0900

    [FE-Seller][Design] 주문 현황 페이지 초기 생성

--- frontend-seller/src/App.tsx
+++ frontend-seller/src/App.tsx
@@ -7,6 +7,7 @@ import LogIn from "./components/login/LogIn.tsx"
 import SignUp from "./components/login/SignUp.tsx";
 import RegisterShop from "./components/shops/RegisterShop.tsx";
 import RegisterFood from "./components/foods/RegisterFood.tsx";
+import ListOrder from "./components/list/ListOrder.tsx";
 
 import MainLayout from "./components/ui/MainLayout.tsx";
 
@@ -21,6 +22,7 @@ const App = () => {
           <Route path="/signup" element={<SignUp />} />
           <Route path="/register/shop" element={<RegisterShop />} />
           <Route path="/register/food" element={<RegisterFood />} />
+          <Route path="/test/main" element={<ListOrder />} />
         </Route>
       </Routes>
     </BrowserRouter>
