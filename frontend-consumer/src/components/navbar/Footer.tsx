ef7ea0aeabb132dd54ffcd2150298c777e63eb0d
Anhyunsung <gustjd980112@gmail.com>
2024-04-25 14:42:33 +0900

    [FE-Consumer][Feat] 상/하단바 및 버튼 Link 기능 추가

--- frontend-consumer/src/components/navbar/Footer.tsx
+++ frontend-consumer/src/components/navbar/Footer.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 import search from "../../assets/footer/search.png";
 import like from "../../assets/footer/like.png";
 import map from "../../assets/footer/map.png";
@@ -7,26 +9,26 @@ import profile from "../../assets/footer/profile.png";
 const Footer = () => {
   return (
     <div className="fixed flex bg-white w-full p-2 bottom-0 border-t-2">
-      <footer>
-        <img className="pl-0.5" src={search} alt="검색" />
-        <div className="footer-div">검색</div>
-      </footer>
-      <footer>
-        <img className="pl-0.5" src={like} alt="찜" />
-        <div className="footer-div">찜</div>
-      </footer>
-      <footer>
-        <img className="pl-0.5" src={map} alt="홈" />
-        <div className="footer-div">홈</div>
-      </footer>
-      <footer>
-        <img className="pl-0.5" src={order} alt="주문내역" />
-        <div className="footer-div">주문내역</div>
-      </footer>
-      <footer>
-        <img src={profile} alt="마이페이지" />
-        <div className="footer-div">마이페이지</div>
-      </footer>
+        <Link className="footer" to="/search">
+          <img src={search} alt="검색" />
+          <div className="footer-div">검색</div>
+        </Link>
+        <Link className="footer" to="/favorite">
+          <img src={like} alt="찜" />
+          <div className="footer-div">찜</div>
+        </Link>
+        <Link className="footer" to="/">
+          <img src={map} alt="홈" />
+          <div className="footer-div">홈</div>
+        </Link>
+        <Link className="footer" to="/order-list">
+          <img src={order} alt="주문내역" />
+          <div className="footer-div">주문내역</div>
+        </Link>
+        <Link className="footer" to="/profile">
+          <img src={profile} alt="마이페이지" />
+          <div className="footer-div">마이페이지</div>
+        </Link>
     </div>
   );
 };
