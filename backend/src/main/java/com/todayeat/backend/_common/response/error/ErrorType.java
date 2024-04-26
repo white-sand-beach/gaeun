cb20fe964bffcee162e39e7fd0460a2c81b91bab
limsubinn <mymyx02@naver.com>
2024-04-26 15:27:09 +0900

    [BE][Style] 에러 타입 띄어쓰기 제거

--- backend/src/main/java/com/todayeat/backend/_common/response/error/ErrorType.java
+++ backend/src/main/java/com/todayeat/backend/_common/response/error/ErrorType.java
@@ -9,10 +9,10 @@ public enum ErrorType {
     /**
      * GLOBAL ERROR
      */
-    METHOD_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "지원되지 않는 Http Method 입니다."),
+    METHOD_NOT_SUPPORTED(HttpStatus.BAD_REQUEST, "지원되지 않는 Http Method입니다."),
     URL_NOT_FOUND(HttpStatus.BAD_REQUEST, "잘못된 URL 입니다."),
-    PATH_VARIABLE_NOT_FOUND(HttpStatus.BAD_REQUEST, "Path Variable 이 없습니다."),
-    REQUEST_PARAM_NOT_FOUND(HttpStatus.BAD_REQUEST, "Request Param 이 없습니다."),
+    PATH_VARIABLE_NOT_FOUND(HttpStatus.BAD_REQUEST, "Path Variable이 없습니다."),
+    REQUEST_PARAM_NOT_FOUND(HttpStatus.BAD_REQUEST, "Request Param이 없습니다."),
 
     /**
      * CUSTOM ERROR
@@ -43,7 +43,7 @@ public enum ErrorType {
 
     // token
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
-    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "Http Header 에 토큰이 없습니다.")
+    TOKEN_NOT_FOUND(HttpStatus.UNAUTHORIZED, "Http Header에 토큰이 없습니다.")
     ;
 
     private HttpStatus httpStatus;
