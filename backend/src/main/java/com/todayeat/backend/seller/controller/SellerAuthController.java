6835905a993965fb3fb07b25b00eb6c9203b9e0e
tddudwns1 <tjddudwns1@naver.com>
2024-04-27 03:03:30 +0900

    [BE][Feat] 판매자 임시 비밀번호 생성 및 인증 구현
    - mail 전송 구현
    1. Google SMTP 서버 이용해서 이메일 인증 진행
    2. 관련 설정 파일 추가
    3. html 형식으로 전송
    - 임시 비밀번호 생성 및 인증 구현
    1. 비밀번호를 유실한 판매자는 인증받은 phoneNumber와, email을 함께 제출해야올바른 email인지 확인 됨
    2. 임시 비밀번호가 인증되지 않았을 때 비밀번호를 업데이트 하면 다른 사용자가 요청한 경우에 비밀번호가 초기화 되므로 인증 전 비밀번호 업데이트 안 함
    - 기타
    1. naming seller 추가
    2. schema 요청 추가
    3. 누락 url 추가

--- backend/src/main/java/com/todayeat/backend/seller/controller/SellerAuthController.java
+++ backend/src/main/java/com/todayeat/backend/seller/controller/SellerAuthController.java
@@ -1,10 +1,9 @@
 package com.todayeat.backend.seller.controller;
 
 import com.todayeat.backend._common.response.success.SuccessResponse;
-import com.todayeat.backend.seller.dto.request.CheckEmailRequest;
-import com.todayeat.backend.seller.dto.request.FindEmailSellerRequest;
-import com.todayeat.backend.seller.dto.request.SignupSellerRequest;
+import com.todayeat.backend.seller.dto.request.*;
 import com.todayeat.backend.seller.dto.response.CheckEmailSellerResponse;
+import com.todayeat.backend.seller.dto.response.CheckTempPasswordSellerResponse;
 import com.todayeat.backend.seller.dto.response.FindEmailSellerResponse;
 import com.todayeat.backend.seller.service.SellerService;
 import lombok.RequiredArgsConstructor;
@@ -27,14 +26,27 @@ public class SellerAuthController implements SellerAuthControllerDocs {
     }
 
     @Override
-    public SuccessResponse<CheckEmailSellerResponse> checkEmail(CheckEmailRequest checkEmailRequest) {
+    public SuccessResponse<CheckEmailSellerResponse> checkEmail(CheckEmailSellerRequest checkEmailSellerRequest) {
 
-        return SuccessResponse.of(sellerService.checkEmail(checkEmailRequest), CHECK_EMAIL_SUCCESS);
+        return SuccessResponse.of(sellerService.checkEmail(checkEmailSellerRequest), CHECK_EMAIL_SUCCESS);
     }
 
     @Override
     public SuccessResponse<FindEmailSellerResponse> fineEmail(FindEmailSellerRequest findEmailSellerRequest) {
 
         return SuccessResponse.of(sellerService.findEmail(findEmailSellerRequest), GET_EMAIL_SUCCESS);
+    }
+
+    @Override
+    public SuccessResponse<Void> getTempPassword(GetTempPasswordSellerRequest getTempPasswordSellerRequest) {
+
+        sellerService.getTempPassword(getTempPasswordSellerRequest);
+        return SuccessResponse.of(GET_TEMP_PASSWORD_SUCCESS);
+    }
+
+    @Override
+    public SuccessResponse<CheckTempPasswordSellerResponse> checkTempPassword(CheckTempPasswordSellerRequest checkTempPasswordSellerRequest) {
+
+        return SuccessResponse.of(sellerService.checkTempPassword(checkTempPasswordSellerRequest), CHECK_TEMP_PASSWORD_SUCCESS);
     }
 }
--- backend/src/main/java/com/todayeat/backend/seller/service/SellerService.java
+++ backend/src/main/java/com/todayeat/backend/seller/service/SellerService.java
@@ -1,12 +1,13 @@
 package com.todayeat.backend.seller.service;
 
 import com.todayeat.backend._common.response.error.exception.BusinessException;
+import com.todayeat.backend._common.util.MailUtil;
 import com.todayeat.backend.seller.dto.SellerCustomUserDetails;
-import com.todayeat.backend.seller.dto.request.CheckEmailRequest;
-import com.todayeat.backend.seller.dto.request.FindEmailSellerRequest;
-import com.todayeat.backend.seller.dto.request.SignupSellerRequest;
+import com.todayeat.backend.seller.dto.request.*;
 import com.todayeat.backend.seller.dto.response.CheckEmailSellerResponse;
+import com.todayeat.backend.seller.dto.response.CheckTempPasswordSellerResponse;
 import com.todayeat.backend.seller.dto.response.FindEmailSellerResponse;
+import com.todayeat.backend.seller.entity.Seller;
 import com.todayeat.backend.seller.mapper.SellerMapper;
 import com.todayeat.backend.seller.repository.SellerRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,10 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.util.Random;
+
 import static com.todayeat.backend._common.response.error.ErrorType.*;
 
 @Slf4j
@@ -26,6 +31,7 @@ public class SellerService implements UserDetailsService {
 
     private final SellerRepository sellerRepository;
     private final PasswordEncoder passwordEncoder;
+    private final MailUtil mailUtil;
 
     public void signup(SignupSellerRequest signupSellerRequest) {
 
@@ -39,12 +45,12 @@ public class SellerService implements UserDetailsService {
         sellerRepository.save(SellerMapper.INSTANCE.signupSellerRequestToSeller(signupSellerRequest, passwordEncoder));
     }
 
-    public CheckEmailSellerResponse checkEmail(CheckEmailRequest checkEmailRequest) {
+    public CheckEmailSellerResponse checkEmail(CheckEmailSellerRequest checkEmailSellerRequest) {
 
-        log.info("checkEmailRequest.getEmail() : {}", checkEmailRequest.getEmail());
+        log.info("checkEmailRequest.getEmail() : {}", checkEmailSellerRequest.getEmail());
 
         return SellerMapper.INSTANCE.toCheckEmailSellerResponse(
-                sellerRepository.existsByEmail(checkEmailRequest.getEmail()));
+                sellerRepository.existsByEmail(checkEmailSellerRequest.getEmail()));
     }
 
     public FindEmailSellerResponse findEmail(FindEmailSellerRequest findEmailSellerRequest) {
@@ -54,6 +60,63 @@ public class SellerService implements UserDetailsService {
         return SellerMapper.INSTANCE.SellerToFindEmailSellerResponse(
                 sellerRepository.findByPhoneNumber(findEmailSellerRequest.getPhoneNumber())
                         .orElseThrow(() -> new BusinessException(PHONE_NUMBER_NOT_FOUND)));
+    }
+
+    public void getTempPassword(GetTempPasswordSellerRequest getTempPasswordSellerRequest) {
+
+        log.info("getTempPasswordSellerRequest.getEmail() : {}", getTempPasswordSellerRequest.getEmail());
+
+        Seller seller = sellerRepository.findByPhoneNumber(getTempPasswordSellerRequest.getPhoneNumber())
+                .orElseThrow(() -> new BusinessException(PHONE_NUMBER_NOT_FOUND));
+
+        if (!seller.getEmail().equals(getTempPasswordSellerRequest.getEmail())) {
+
+            throw new BusinessException(EMAIL_UNAUTHORIZED);
+        }
+
+        sendCertification(getTempPasswordSellerRequest.getEmail());
+    }
+
+    public CheckTempPasswordSellerResponse checkTempPassword(CheckTempPasswordSellerRequest checkTempPasswordSellerRequest) {
+
+        log.info("checkTempPasswordSellerRequest.getTempPassword() : {}", checkTempPasswordSellerRequest.getTempPassword());
+
+        if (sellerRepository.existsByEmail(checkTempPasswordSellerRequest.getEmail())) {
+
+            throw new BusinessException(EMAIL_CONFLICT);
+        }
+
+        // todo redis에서 비교
+        
+        // todo 임시 비밀번호로 비밀번호 업데이트
+
+        return null;
+    }
+
+    private void sendCertification(String email) {
+
+        String title = "today-eat 이메일 인증 번호";
+        String authCode = createCode();
+
+        log.info("authCode : {}", authCode);
+
+        mailUtil.sendEmail(email, title, "<h3>인증코드</h3>" + authCode);
+
+        // todo redis에 저장
+    }
+
+    private String createCode() {
+        int length = 6;
+        try {
+            Random random = SecureRandom.getInstanceStrong();
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i < length; i++) {
+                builder.append(random.nextInt(10));
+            }
+            return builder.toString();
+        } catch (NoSuchAlgorithmException e) {
+            throw new BusinessException(MAIL_INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Override
