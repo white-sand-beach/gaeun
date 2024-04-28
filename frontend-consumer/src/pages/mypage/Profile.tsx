fbdc3da45649d43566310886f4639934bd03da43
jinyoung <kimyoung100@naver.com>
2024-04-29 01:18:37 +0900

    [FE-Consumer][Design] 마이페이지 프로필 하단부분 화면구성

--- frontend-consumer/src/pages/mypage/Profile.tsx
+++ frontend-consumer/src/pages/mypage/Profile.tsx
@@ -1,7 +1,7 @@
 const ProfileCard = () => {
   return (
     <div className="w-full h-screen bg-white border border-black">
-      <div className="flex items-center justify-center w-11/12 pt-16 m-auto">
+      <div className="flex items-center justify-center w-11/12 pt-20 m-auto">
         <div className="relative w-full h-56 p-6 m-auto bg-white border border-gray-500 rounded-lg shadow-lg">
           {/* 설정 아이콘 */}
           <div className="absolute right-4 top-4">
@@ -37,6 +37,30 @@ const ProfileCard = () => {
               📅
             </button>
           </div>
+        </div>
+      </div>
+      <div className="w-11/12 mx-auto mt-5">
+        <div className="p-4 mb-5 text-center bg-white border border-gray-500 rounded-lg shadow-lg">
+          <span className="text-sm text-gray-800">
+            음식을 지키고 식물을 키워주세요!!
+          </span>
+        </div>
+        <div className="relative h-64 p-4 bg-white border border-gray-500 rounded-lg shadow-lg">
+          {/* 왼쪽 상단 문구 */}
+          <span className="absolute text-sm text-gray-800 top-4 left-4">
+            팽둔님이 지금까지 지킨 음식은?
+          </span>
+          {/* 중앙 아래 식물 이미지 */}
+          <div className="absolute transform -translate-x-1/2 bottom-4 left-1/2">
+            <div className="inline-block p-2 bg-green-100 rounded-full">
+              {/* 아이콘 또는 이미지 */}
+              <span className="text-green-800">식물</span>
+            </div>
+          </div>
+          {/* 오른쪽 하단 금액 */}
+          <span className="absolute text-lg text-blue-600 bottom-4 right-4">
+            10,000원
+          </span>
         </div>
       </div>
     </div>
