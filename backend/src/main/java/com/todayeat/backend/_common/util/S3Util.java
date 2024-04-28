e18144e668967679aae8a9f24a68755a808e51e9
kdozlo <eodud5730@naver.com>
2024-04-29 00:43:48 +0900

    [BE][Fix] 예외 처리 및 이미지 업로드시 디렉토리 경로 설정 방법 변경
    1. 이미지 파일 형식이 아닌 경우 예외 처리
    2. 이미지 파일 s3에 올릴 때 AmazonServiceException 예외 처리
    3. String 형태로 직접 받았던것을 enum 타입의 DirectoryType로 받는 방법으로 변경
    
    #S10P31D104-97

--- backend/src/main/java/com/todayeat/backend/_common/util/S3Util.java
+++ backend/src/main/java/com/todayeat/backend/_common/util/S3Util.java
@@ -1,8 +1,13 @@
 package com.todayeat.backend._common.util;
 
+import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.todayeat.backend._common.entity.DirectoryType;
+import com.todayeat.backend._common.response.error.ErrorType;
+import com.todayeat.backend._common.response.error.exception.BusinessException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
@@ -11,6 +16,7 @@ import java.io.IOException;
 import java.util.Objects;
 import java.util.UUID;
 
+@Slf4j
 @Component
 @RequiredArgsConstructor
 public class S3Util {
@@ -20,31 +26,34 @@ public class S3Util {
     @Value("${cloud.aws.s3.bucket}")
     private String bucket;
 
-    public String upload(MultipartFile multipartFile, String dirNamePrincipal, String dirNamePrincipalId, String dirNameAttribute) throws IOException {
-        // 이미지 형식의 파일인지 확인
+    public String upload(MultipartFile multipartFile, DirectoryType directoryType, Long dirNamePrincipalId) {
+        //이미지 형식의 파일인지 확인
         if(!Objects.requireNonNull(multipartFile.getContentType()).contains("image")) {
-            //ToDo : 에러 처리 하기
+            throw new BusinessException(ErrorType.IMAGE_FORMAT_INVALID);
         }
 
-        // 새로운 파일 이름 생성
-        String fileName = createFileName(multipartFile, createDirName(dirNamePrincipal, dirNamePrincipalId, dirNameAttribute));
+        //새로운 파일 이름 생성
+        String fileName = createFileName(multipartFile, createDirName(directoryType, dirNamePrincipalId));
 
-        // 메타데이터 설정
-        ObjectMetadata metadata = createMetadata(multipartFile);
-
+        //s3 버킷에 이미지 객체 저장
         try {
-            amazonS3.putObject(bucket, fileName, multipartFile.getInputStream(), metadata);
-        } catch (IOException e) {
-            //ToDo : 에러 처리 하기
+            amazonS3.putObject(bucket, fileName, multipartFile.getInputStream(), createMetadata(multipartFile));
+        } catch (AmazonServiceException | IOException e) {
+            log.error("S3Util upload putObject error : ", e);
+            throw new BusinessException(ErrorType.INTERNAL_SERVER_ERROR);
         }
 
+        //이미지 객체 url 반환
         return amazonS3.getUrl(bucket, fileName).toString();
+
     }
 
-    private String createDirName( String dirNamePrincipal, String dirNamePrincipalId, String dirNameAttribute) {
-        return dirNamePrincipal + "/" + dirNamePrincipalId + "/" + dirNameAttribute;
+    //저장할 파일의 경로 생성
+    private String createDirName(DirectoryType directoryType, Long dirNamePrincipalId) {
+        return directoryType.getDirNamePrincipal() + "/" + dirNamePrincipalId + "/" + directoryType.getDirNameAttribute();
     }
 
+    //저장할 파일의 메타 데이터 생성
     private ObjectMetadata createMetadata (MultipartFile multipartFile) {
         ObjectMetadata metadata = new ObjectMetadata();
         metadata.setContentLength(multipartFile.getSize());
@@ -53,8 +62,8 @@ public class S3Util {
         return metadata;
     }
 
+    //저장할 파일의 이름 생성
     private String createFileName(MultipartFile multipartFile, String dirName) {
         return dirName + "/" + UUID.randomUUID() + "_" + multipartFile.getOriginalFilename();
     }
-
 }
