43c9588dc75f0d7ee47e9b842623fda5af1e93f8
parkjonghyuk <occultism22@naver.com>
2024-04-29 15:22:07 +0900

    [FE-Seller][Feat] 주문현황 페이지 컴포넌트 생성

---
+++ frontend-seller/src/pages/list/OrderlistPage.tsx
@@ -0,0 +1,30 @@
+const OrderListPage = () => {
+    const orderLists = [
+        {
+            orderNum: "1",
+            orderDate: "2024-04-26",
+            foodName: "햄버거1",
+            price: 13900,
+        },
+        {
+            orderNum: "2",
+            orderDate: "2024-04-26",
+            foodName: "햄버거2",
+            price: 23900,
+        },
+        {
+            orderNum: "3",
+            orderDate: "2024-04-26",
+            foodName: "햄버거3",
+            price: 33900,
+        },
+    ]
+
+    return (
+        <div className="sm:sm-list-view lg:normal-list-view">
+
+        </div>
+    );
+};
+
+export default OrderListPage;
