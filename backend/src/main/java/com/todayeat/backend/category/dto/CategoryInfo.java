96a31a146040c58c264f1c55b482f3e99711018d
tddudwns1 <tjddudwns1@naver.com>
2024-05-01 02:32:54 +0900

    [BE][Feat] 가게 카테고리 기능 구현
    - 가게 생성 로직에 카테고리 목록 추가
    
    - 가게 조회 로직에 카테고리 목록 추가
    1. 응답 카테고리 dto 분리
    2. 가게 카테고리 mapping을 위해 내부에 Builder 추가
    3. 중복되는 가게 검색 메서드화
    
    - 가게 카테고리 수정 로직 구현 : 카테고리 수정마다 기존 카테고리 제거 후 생성하면 비효율적인 문제가 생겨서 기존 카테고리와 비교
    1. 이미 존재하는 카테고리 목록 생성
    2. 카테고리 목록 중 입력 받은 목록에 없는 카테고리는 제거
    3. 입력 받은 목록 중 카테고리 목록에 없는 카테고리는 추가
    
    - 기타
    1. storeController swagger 에러 응답 수정
    2. 판매자 가게 주입을 위해 fetchType Lazy에서 Eager로 수정
    3. storeCategory delete문 table 오류 수정
    4. 가게 생성 로직에서 save 누락으로 인해 판매자 테이블에 업데이트 되지 않던 오류 수정

---
+++ backend/src/main/java/com/todayeat/backend/category/dto/CategoryInfo.java
@@ -0,0 +1,12 @@
+package com.todayeat.backend.category.dto;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CategoryInfo {
+    private String name;
+    private String image;
+}
--- backend/src/main/java/com/todayeat/backend/store/service/StoreService.java
+++ backend/src/main/java/com/todayeat/backend/store/service/StoreService.java
@@ -3,8 +3,14 @@ package com.todayeat.backend.store.service;
 import com.todayeat.backend._common.response.error.exception.BusinessException;
 import com.todayeat.backend._common.util.S3Util;
 import com.todayeat.backend._common.util.SecurityUtil;
+import com.todayeat.backend.category.entity.StoreCategory;
+import com.todayeat.backend.category.mapper.CategoryMapper;
+import com.todayeat.backend.category.mapper.StoreCategoryMapper;
+import com.todayeat.backend.category.repository.CategoryRepository;
+import com.todayeat.backend.category.repository.StoreCategoryRepository;
 import com.todayeat.backend.location.entity.Coordinate;
 import com.todayeat.backend.seller.entity.Seller;
+import com.todayeat.backend.seller.repository.SellerRepository;
 import com.todayeat.backend.store.dto.request.CreateStoreRequest;
 import com.todayeat.backend.store.dto.request.UpdateStoreRequest;
 import com.todayeat.backend.store.dto.response.GetConsumerDetailStoreResponse;
@@ -18,9 +24,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Objects;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import static com.todayeat.backend._common.entity.DirectoryType.SELLER_STORE_IMAGE;
+import static com.todayeat.backend._common.response.error.ErrorType.CATEGORY_NOT_FOUND;
 import static com.todayeat.backend._common.response.error.ErrorType.STORE_NOT_FOUND;
 
 @Slf4j
@@ -29,6 +39,9 @@ import static com.todayeat.backend._common.response.error.ErrorType.STORE_NOT_FO
 @RequiredArgsConstructor
 public class StoreService {
 
+    private final StoreCategoryRepository storeCategoryRepository;
+    private final CategoryRepository categoryRepository;
+    private final SellerRepository sellerRepository;
     private final StoreRepository storeRepository;
 
     private final SecurityUtil securityUtil;
@@ -41,6 +54,13 @@ public class StoreService {
         Store store = storeRepository.save(StoreMapper.INSTANCE.createStoreRequestToStore(createStoreRequest, seller.getId(), s3Util));
 
         seller.updateStore(store);
+        sellerRepository.save(seller);
+
+        createStoreRequest.getCategoryIdList().stream()
+                .map(categoryId -> categoryRepository.findById(categoryId)
+                        .orElseThrow(() -> new BusinessException(CATEGORY_NOT_FOUND)))
+                .map(category -> StoreCategoryMapper.INSTANCE.toStoreCategory(store, category))
+                .forEach(storeCategoryRepository::save);
     }
 
     public GetSellerStoreResponse getSellerStore(Long storeId) {
@@ -52,14 +72,20 @@ public class StoreService {
             throw new BusinessException(STORE_NOT_FOUND);
         }
 
-        return StoreMapper.INSTANCE.storeToGetSellerStoreResponse(store);
+        GetSellerStoreResponse getSellerStoreResponse = StoreMapper.INSTANCE.storeToGetSellerStoreResponse(store);
+
+        getSellerStoreResponse.setCategoryList(
+                categoryRepository.findAll().stream()
+                        .map(CategoryMapper.INSTANCE::categoryToCategoryInfo)
+                        .collect(Collectors.toList()));
+
+        return getSellerStoreResponse;
     }
 
     public GetConsumerInfoStoreResponse getConsumerInfoStore(Long storeId) {
 
         GetConsumerInfoStoreResponse getStoreConsumerResponse = StoreMapper.INSTANCE.storeToGetConsumerStoreResponse(
-                storeRepository.findByIdAndDeletedAtIsNull(storeId)
-                        .orElseThrow(() -> new BusinessException(STORE_NOT_FOUND)));
+                validateAndGetStore(storeId));
 
         //todo getDetailStoreConsumerResponse에 찜 여부 넣어야 함
 
@@ -69,8 +95,7 @@ public class StoreService {
     public GetConsumerDetailStoreResponse getConsumerDetailStore(Long storeId) {
 
         return StoreMapper.INSTANCE.storeToGetConsumerDetailStoreResponse(
-                storeRepository.findByIdAndDeletedAtIsNull(storeId)
-                        .orElseThrow(() -> new BusinessException(STORE_NOT_FOUND)));
+                validateAndGetStore(storeId));
     }
 
     @Transactional
@@ -95,6 +120,21 @@ public class StoreService {
                 updateStoreRequest.getOriginCountry(),
                 updateStoreRequest.getIntroduction()
         );
+        storeRepository.save(store);
+
+        List<StoreCategory> existingCategories = storeCategoryRepository.findByStoreIdAndDeletedAtIsNull(storeId);
+        Set<Long> existingCategoryIds = existingCategories.stream()
+                .map(sc -> sc.getCategory().getId())
+                .collect(Collectors.toSet());
+
+        existingCategories.stream()
+                .filter(sc -> !updateStoreRequest.getCategoryIdList().contains(sc.getCategory().getId()))
+                .forEach(storeCategoryRepository::delete);
+
+        updateStoreRequest.getCategoryIdList().stream()
+                .filter(id -> !existingCategoryIds.contains(id))
+                .forEach(id -> storeCategoryRepository.save(
+                        StoreCategoryMapper.INSTANCE.toStoreCategory(store, categoryRepository.findById(id).get())));
     }
 
     @Transactional
@@ -108,5 +148,10 @@ public class StoreService {
         }
 
         store.updateIsOpened();
+    }
+
+    private Store validateAndGetStore(Long storeId) {
+        return storeRepository.findByIdAndDeletedAtIsNull(storeId)
+                .orElseThrow(() -> new BusinessException(STORE_NOT_FOUND));
     }
 }
