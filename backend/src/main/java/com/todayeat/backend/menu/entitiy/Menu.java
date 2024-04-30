84cc43084b663f02fdfc13a71338db6d419447a4
kdozlo <eodud5730@naver.com>
2024-04-30 18:05:14 +0900

    [BE][Feat] 엔티티 작성
    Menu, OrderInfo, Review, Sale
    
    #S10P31D104-81

---
+++ backend/src/main/java/com/todayeat/backend/menu/entitiy/Menu.java
@@ -0,0 +1,43 @@
+package com.todayeat.backend.menu.entitiy;
+
+import com.todayeat.backend._common.entity.BaseTime;
+import com.todayeat.backend.store.entity.Store;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE menu SET deleted_at = CONVERT_TZ(NOW(), '+00:00', '+09:00') WHERE menu_id = ?")
+public class Menu extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "menu_id")
+    private Long id;
+
+    @Column(nullable = true)
+    private String imageUrl;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer originalPrice;
+
+    @Column(nullable = false)
+    private Integer sellPrice;
+
+    @Column(nullable = false)
+    private Integer discountRate;
+
+    @Column(nullable = false)
+    private Integer sequence; // 화면에 보이는 메뉴 목차 순서
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", referencedColumnName = "store_id")
+    private Store store;
+}
---
+++ backend/src/main/java/com/todayeat/backend/review/entity/Review.java
@@ -0,0 +1,38 @@
+package com.todayeat.backend.review.entity;
+
+
+import com.todayeat.backend._common.entity.BaseTime;
+import com.todayeat.backend.consumer.entity.Consumer;
+import com.todayeat.backend.orderinfo.entity.OrderInfo;
+import com.todayeat.backend.store.entity.Store;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@SQLDelete(sql = "UPDATE review SET deleted_at = CONVERT_TZ(NOW(), '+00:00', '+09:00') WHERE review_id = ?")
+public class Review extends BaseTime {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_id")
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String content;
+
+    @Column(nullable = true)
+    private String imageUrl;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "consumer_id", referencedColumnName = "consumer_id")
+    private Consumer consumer;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id", referencedColumnName = "store_id")
+    private Store store;
+}
