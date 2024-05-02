6797aeac56f836e50cdb327c7f9ae8692a6b9aa9
parkjonghyuk <occultism22@naver.com>
2024-05-02 17:22:50 +0900

    [FE-Seller][Design] Header, Footer CSS값 수정

--- frontend-seller/src/components/ui/Footer.tsx
+++ frontend-seller/src/components/ui/Footer.tsx
@@ -9,23 +9,23 @@ const Footer = () => {
         <footer>
             <div className="flex flex-col items-center justify-center">
                 <img src={ChartIcon} alt="통계" />
-                <p className="text-[16px] text-gray-500">통계</p>
+                <p>통계</p>
             </div>
             <div className="flex flex-col items-center justify-center">
                 <img src={SellListIcon} alt="판매내역" />
-                <p className="text-[16px] text-gray-500">판매내역</p>
+                <p>판매내역</p>
             </div>
             <div className="flex flex-col items-center justify-center">
                 <img src={OrderListIcon} alt="주문현황" />
-                <p className="text-[16px] text-gray-500">주문현황</p>
+                <p>주문현황</p>
             </div>
             <div className="flex flex-col items-center justify-center">
                 <img src={AddProductIcon} alt="물품등록" />
-                <p className="text-[16px] text-gray-500">등록하기</p>
+                <p>등록하기</p>
             </div>
             <div className="flex flex-col items-center justify-center">
                 <img src={ProfileIcon} alt="프로필" />
-                <p className="text-[16px] text-gray-500">프로필</p>
+                <p>프로필</p>
             </div>
         </footer>
     );
