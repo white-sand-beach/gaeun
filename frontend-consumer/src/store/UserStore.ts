9132da70a3a6d8f97b053f4e0579f3fdadbe6f0a
Anhyunsung <gustjd980112@gmail.com>
2024-05-02 17:42:30 +0900

    [FE-Consumer][Feat] UserStore 수정

--- frontend-consumer/src/store/UserStore.ts
+++ frontend-consumer/src/store/UserStore.ts
@@ -1,27 +1,48 @@
 import { create } from "zustand";
 import { persist, createJSONStorage, PersistOptions } from "zustand/middleware";
+import Cookies from 'universal-cookie';
 
-import UserState from "../types/UserState"
+import CookieState from "../types/CookieState";
 
 // PersistOptions은 
-// persist 함수에 전달되는 옵션 타입을 명확하게 정의합니다!
-// ( 코드의 가독성 및 유지보수성 향상 )
+// persist 함수에 전달되는 옵션 타입을 명확하게 정의합니다! 
+// ( 코드의 가독성 및 유지보수성 향상 ) 
+
+const cookies = new Cookies();
 
 // 상태 함수 정의 !
-const useUserStore = create<UserState>()(
+const useUserStore = create<CookieState>()(
   persist(
     (set) => ({
-      email: "",
-      nickName: "",
-      profileImg: "",
-      phoneNumber: "",
+      email: '',
+      nickName: '',
+      profileImg: '',
+      phoneNumber: '',
+      accessToken: cookies.get('accessToken') || '',
       updateUserState: (key, value) => set((state) => ({ ...state, [key]: value })),
+      setAccessToken: (token: string) => {
+        cookies.set('accessToken', token, { path: '/', secure: true, httpOnly: true });
+        set((state) => ({ ...state, accessToken: token }));
+      },
     }),
     {
-      name: "user-storage",
+      name: 'user-storage',
       storage: createJSONStorage(() => localStorage),
-    } as PersistOptions<UserState>
+      partialize: (state) => ({
+        email: state.email,
+        nickName: state.nickName,
+        profileImg: state.profileImg,
+        phoneNumber: state.phoneNumber,
+      }),
+    } as PersistOptions<CookieState>
   )
 );
+
+// 쿠키(액세스 토큰 값) 변경(예: 재발급) 시 스토어 업데이트
+cookies.addChangeListener((change) => {
+  if (change.name === "accessToken") {
+    useUserStore.getState().setAccessToken(change.value);
+  } 
+})
 
 export default useUserStore;
--- frontend-consumer/src/types/UserState.ts
+++ frontend-consumer/src/types/UserState.ts
@@ -4,12 +4,15 @@ interface UserState {
   nickName?: string;
   profileImg?: string;
   phoneNumber?: string;
-  
+
   // K는 제네릭 타입 변수
   // UserStore의 모든 키를 유니온 타입으로 반환
   // (key: K, value: UserStore[K]): 함수의 인자로 key와 value를 받음
   // key는 UserStore의 키 중 하나이며, value는 key에 해당하는 UserStore의 값의 타입과 동일해야함
-  updateUserState?: <K extends keyof UserState>(key: K, value: UserState[K]) => void;
+  updateUserState?: <K extends keyof UserState>(
+    key: K,
+    value: UserState[K]
+  ) => void;
 }
 
 export default UserState;
