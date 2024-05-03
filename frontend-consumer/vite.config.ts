697912c570b919428ea34dd1fa4dfb2958494e98
kdozlo <eodud5730@naver.com>
2024-05-03 15:34:30 +0900

    [FE][Chore] vite.config.ts 수정

--- frontend-consumer/vite.config.ts
+++ frontend-consumer/vite.config.ts
@@ -5,7 +5,6 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/consumer",
   plugins: [
     million.vite({ auto: true }),
     react(),
@@ -61,7 +60,7 @@ export default defineConfig({
       },
     }),
   ],
-  // server: {
-  //   open: "/consumer",
-  // },
+  server: {
+    open: "/consumer",
+  },
 });
