6d0d98a133990e1906224178a64819e733dbeec7
parkjonghyuk <occultism22@naver.com>
2024-05-03 16:11:36 +0900

    [FE-Seller][Chore] vite.config.ts 수정 base URL 설정

--- frontend-seller/vite.config.ts
+++ frontend-seller/vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/seller",
   plugins: [
     react(),
     VitePWA({
@@ -66,7 +67,6 @@ export default defineConfig({
     })
   ],
   server: {
-    open: "/seller",
     port: 5174
   },
 })
