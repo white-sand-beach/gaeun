4fd11a808dd18fc66b834b908bfc4ecd5039458f
parkjonghyuk <occultism22@naver.com>
2024-05-03 14:29:55 +0900

    [FE-Seller][Chore] vite.cofing.ts base-url 설정

--- frontend-seller/vite.config.ts
+++ frontend-seller/vite.config.ts
@@ -4,6 +4,7 @@ import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "./seller",
   plugins: [
     react(),
     VitePWA({
@@ -68,5 +69,5 @@ export default defineConfig({
   server: {
     open: "/seller",
     port: 5174
-  }
+  },
 })
