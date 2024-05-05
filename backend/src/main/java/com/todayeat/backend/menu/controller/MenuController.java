5c14ce49d7c1c1d670123434a41f581defc304c6
kdozlo <eodud5730@naver.com>
2024-05-06 03:14:14 +0900

    [BE][Fix] MenuController, MenuControllerDocs 수정
    - MenuControllerDocs에는 스웨거 관련 정보만 들어있도록 수정
    
    #S10P31D104-119

--- backend/src/main/java/com/todayeat/backend/menu/controller/MenuController.java
+++ backend/src/main/java/com/todayeat/backend/menu/controller/MenuController.java
@@ -7,17 +7,21 @@ import com.todayeat.backend.menu.service.MenuService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/api/menus")
 public class MenuController implements MenuControllerDocs{
 
     private final MenuService menuService;
 
     @Override
     @PreAuthorize("hasRole('SELLER')")
+    @PostMapping
     public SuccessResponse<Void> create(CreateMenuRequest request) {
 
         menuService.create(request);
--- backend/src/main/java/com/todayeat/backend/menu/controller/MenuControllerDocs.java
+++ backend/src/main/java/com/todayeat/backend/menu/controller/MenuControllerDocs.java
@@ -9,12 +9,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Tag(name = "menus", description = "가게 메뉴")
-@RequestMapping("/api/menus")
 public interface MenuControllerDocs {
 
     @Operation(summary = "메뉴 등록",
@@ -27,6 +24,5 @@ public interface MenuControllerDocs {
     @ApiResponse(responseCode = "404",
             description = "판매자의 가게가 맞는지 확인",
             content = @Content(schema = @Schema(implementation = ErrorResponse.class)))
-    @PostMapping
     SuccessResponse<Void> create(@RequestBody @Valid CreateMenuRequest request);
 }
