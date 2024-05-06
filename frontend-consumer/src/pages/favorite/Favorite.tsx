d704404a1bf22d9077d38c19a2a0ac41ce625528
안현성 <gustjd980112@gmail.com>
2024-05-06 23:52:47 +0900

    [Fe-Consumer][Fix] Get, Post API 오류 코드 수정 및 모든 API 연결

--- frontend-consumer/src/pages/favorite/Favorite.tsx
+++ frontend-consumer/src/pages/favorite/Favorite.tsx
@@ -1,16 +1,62 @@
+import { useEffect, useState } from "react";
 import FavoriteList from "../../components/favorite/FavoriteList";
+import FavoriteGetForm from "../../services/favorites/FavoriteGetService";
+import { FavoriteResponse, FavoriteState  } from "../../types/FavoriteType";
 
 const Favorite = () => {
+  const [favoriteState, setFavoriteState] = useState<FavoriteState>({
+    favorites: [],
+    totalCnt: 0,
+    page: 0,
+    loading: false,
+    hasNext: false,
+  });
+
+  useEffect(() => {
+    const fetchFavorites = async () => {
+      setFavoriteState((prevState) => ({ ...prevState, loading: true }));
+      try {
+        const response: FavoriteResponse = await FavoriteGetForm({ page: favoriteState.page, size: 10 });
+        setFavoriteState((prevState) => ({
+          ...prevState,
+          favorites: [...prevState.favorites, ...response.data.favorites || []],
+          totalCnt: response.data.totalCnt,
+          hasNext: response.data.hasNext,
+        }));
+      } catch (error) {
+        console.error("에러발생 에러발생! ", error);
+      } finally {
+        setFavoriteState((prevState) => ({ ...prevState, loading: false }));
+      }
+    };
+
+    fetchFavorites();
+  }, [favoriteState.page]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollHeight = document.documentElement.scrollHeight;
+      const scrollTop = document.documentElement.scrollTop;
+      const clientHeight = document.documentElement.clientHeight;
+
+      if (scrollTop + clientHeight >= scrollHeight && !favoriteState.loading && favoriteState.hasNext) {
+        setFavoriteState((prevState) => ({ ...prevState, page: prevState.page + 1 }));
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [favoriteState.loading, favoriteState.hasNext]);
+
   return (
     <div className="pt-14 bg-gray-100">
       <div className="flex items-center border-2 border-gray-200">
         <p className="p-2 text-sm font-bold">내가 찜한 맛집</p>
-        <p className="text-xs text-gray-500 font-bold">2개</p>
+        <p className="text-xs text-gray-500 font-bold">{favoriteState.totalCnt}개</p>
       </div>
       <div className="bg-white">
-
         {/* 찜 리스트 */}
-        <FavoriteList />
+        <FavoriteList favorites={favoriteState.favorites} />
       </div>
     </div>
   );
--- frontend-consumer/src/services/favorites/FavoriteGetService.ts
+++ frontend-consumer/src/services/favorites/FavoriteGetService.ts
@@ -1,14 +1,14 @@
 import axios from "axios";
 import Cookies from "universal-cookie";
 
-const FavoriteGetForm = async ({ storeId }: { storeId: number }): Promise<any> => {
+const FavoriteGetForm = async ({ page, size }: {  page?: number, size?: number }): Promise<any> => {
   const cookies = new Cookies();
   const accessToken = cookies.get("accessToken")
   const API_BASE_URL = import.meta.env.VITE_API_URL;
 
   const response = await axios.get(
     `${API_BASE_URL}/api/favorites`, {
-    params: { storeId },
+    params: {  page, size },
     withCredentials: true,
     headers: {
       "Content-Type": "application/json",
