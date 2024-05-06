6556aaaad1eb33d899b3140205acad62d335422c
Park Jong Hyuk <occultism22@naver.com>
2024-05-06 23:43:10 +0900

    [FE-Seller][Add] 마이페이지에 필요한 아이콘 이미지파일 추가

--- frontend-seller/src/pages/user/MyPage.tsx
+++ frontend-seller/src/pages/user/MyPage.tsx
@@ -1,6 +1,15 @@
+import testImg from "../../assets/store-test-img.png"
+
 const MyPage = () => {
   return (
-    <div>
+    <div className="yes-footer top-[60px] gap-3">
+      <img src={testImg} alt="가게 대표 이미지" className="w-full h-[400px]"/>
+      <p>가게 상호명</p>
+      <div className="flex flex-row justify-between">
+        <div>
+          <img src="" alt="리뷰조회" />
+        </div>
+      </div>
     </div>
   );
 };
