9a9a7f1ff071ec2241e8638ec05e4589cfe5fb7a
limsubinn <mymyx02@naver.com>
2024-05-06 19:13:22 +0900

    [FE-Seller][Chore] index.html, vite.config.ts 상대경로로 변경

--- frontend-seller/vite.config.ts
+++ frontend-seller/vite.config.ts
@@ -22,43 +22,43 @@ export default defineConfig({
         theme_color: "#FFAF38",
         icons: [
           {
-            src: "/seller/icons/size-16.png",
+            src: "./icons/size-16.png",
             type: "image/png",
             sizes: "16x16"
           },
           {
-            src: "/seller/icons/size-48.png",
+            src: "./icons/size-48.png",
             type: "image/png",
             sizes: "48x48"
           },
           {
-            src: "/seller/icons/size-72.png",
+            src: "./icons/size-72.png",
             type: "image/png",
             sizes: "72x72"
           },
           {
-            src: "/seller/icons/size-128.png",
+            src: "./icons/size-128.png",
             type: "image/png",
             sizes: "128x128"
           },
           {
-            src: "/seller/icons/size-144.png",
+            src: "./icons/size-144.png",
             type: "image/png",
             sizes: "144x144"
           },
           {
-            src: "/seller/icons/size-152.png",
+            src: "./icons/size-152.png",
             type: "image/png",
             sizes: "152x152"
           },
           {
-            src: "/seller/icons/size-192.png",
+            src: "./icons/size-192.png",
             type: "image/png",
             sizes: "192x192",
             purpose: "maskable"
           },
           {
-            src: "/seller/icons/size-512.png",
+            src: "./icons/size-512.png",
             type: "image/png",
             sizes: "512x512",
             purpose: "maskable"
