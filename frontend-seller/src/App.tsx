5e96258d5564d41063acc083426da200636746a0
parkjonghyuk <occultism22@naver.com>
2024-05-09 12:54:37 +0900

    [FE-Seller][Remove] 회원가입 완료 페이지 컴포넌트 삭제 modal로 대체 예정

--- frontend-seller/src/App.tsx
+++ frontend-seller/src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Main from "./components/ui/Main.tsx";
 import LogIn from "./pages/user/LogInPage.tsx"
 import SignUp from "./pages/user/SignupPage.tsx";
-import SignupFinish from "./components/login/SignupFinish.tsx";
 import RegisterFood from "./components/foods/RegisterFood.tsx";
 import MainLayout from "./components/ui/MainLayout.tsx";
 
@@ -24,7 +23,6 @@ const App = () => {
           <Route path="/" element={<Main />} />
           <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="/signupFin" element={<SignupFinish />} />
           <Route path="/register/shop" element={<RegisterShopPage />} />
           <Route path="/register/food" element={<RegisterFood />} />
           <Route path="/order" element={<OrderListPage />} />
