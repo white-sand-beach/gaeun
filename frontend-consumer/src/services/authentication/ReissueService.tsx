2e9360e059b3b8fad2a387862c0d998b1f3e21cf
Anhyunsung <gustjd980112@gmail.com>
2024-05-10 15:59:37 +0900

    [FE-Consumer][Feat] 리프레쉬 토큰 재발급 로직 수정

--- frontend-consumer/src/services/authentication/ReissueService.tsx
+++ frontend-consumer/src/services/authentication/ReissueService.tsx
@@ -1,20 +1,23 @@
-import axiosInstance from "./AxiosSet";
+import axios from "axios";
 import Cookies from "universal-cookie";
 import TokenState from "../../types/TokenState ";
 
 const ReissueService = async (): Promise<TokenState> => {
   const cookies = new Cookies();
-  const response = await axiosInstance.post(
+  const accessToken = cookies.get("accessToken");
+  const response = await axios.post(
     `${import.meta.env.VITE_API_URL}/api/reissue`,
-    null,
+    {
+      withCredentials: true,
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    }
   );
   cookies.remove("accessToken", { path: "/" });
-  const accessToken = response.headers["Authorization"];
-  cookies.set("accessToken", accessToken, { path: "/" });
-  console.log(
-    "토큰 재발급에 성공하였습니다",
-    response.headers["authorization"]
-  );
+  const NewAccessToken = response.headers["Authorization"];
+  cookies.set("accessToken", NewAccessToken, { path: "/" });
+  console.log("토큰 재발급에 성공하였습니다", NewAccessToken);
   return response.data;
 };
 
--- frontend-consumer/src/services/categories/CategoryService.ts
+++ frontend-consumer/src/services/categories/CategoryService.ts
@@ -2,10 +2,12 @@ import axiosInstance from "../authentication/AxiosSet";
 import { CategoryResponse } from "../../types/CategoryType";
 
 const CategoryForm = async (): Promise<CategoryResponse> => {
-
   const response = await axiosInstance.get(
     `${import.meta.env.VITE_API_URL}/api/categories`);
-  return response.data.data;
+    if (!response.data || !response.data.data) {
+      throw new Error("Invalid response structure");
+    }
+    return response.data.data;
 }
 
 export default CategoryForm;
