4dd04328f2e801a7c3a36f8d5643bf64b0d807a3
parkjonghyuk <occultism22@naver.com>
2024-05-14 10:51:01 +0900

    [FE-Seller][Add] 가게 영업 여부 수정 api 컴포넌트 파일 생성

---
+++ frontend-seller/src/service/shop/OpenShopAPI.ts
@@ -0,0 +1,31 @@
+import axios from "axios";
+import Cookies from "universal-cookie";
+
+const OpenShopAPI = () => {
+    const cookies = new Cookies()
+    const accessToken = cookies.get("accessToken")
+    const storeId = 12
+    const putShopOpened = () => {
+        axios.put(import.meta.env.VITE_BASE_URL + `/api/stores/${storeId}/is-opened`, {}, {
+            params: {
+                "store-id": storeId
+            },
+            headers: {
+                Authorization: `Bearer ${accessToken}`
+            }
+        })
+        .then(res => {
+            console.log(res);
+            console.log("가게 영업 여부 수정 성공");
+        })
+        .catch(err => {
+            console.error(err);
+            console.log("가게 영업 여부 수정 실패");
+        });
+    };
+    return {
+        putShopOpened,
+    };
+};
+
+export default OpenShopAPI;
