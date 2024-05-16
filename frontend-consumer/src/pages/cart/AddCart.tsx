fe3ed719bb8e0caedbcd8ff14ca7fc90d45cb5ac
jinyoung <kimyoung100@naver.com>
2024-05-16 14:09:08 +0900

    [FE-Consumer][Feat] 서비스배너 자동 변환

--- frontend-consumer/src/pages/cart/AddCart.tsx
+++ frontend-consumer/src/pages/cart/AddCart.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import CartInfo from "../../components/cart/CartInfo";
-import ServiceBanner from "../../components/navbar/ServiceBanner";
+import BannerSlider from "../../components/navbar/ServiceBanner";
 import FirstCountButton from "../../components/button/FirstCountButton";
 import AddCartButton from "../../components/button/AddCartButton";
 import MenuDetailGetService from "../../services/carts/MenuDetailGetService";
@@ -11,7 +11,10 @@ import { MenuItem } from "../../types/ShopMenuType";
 
 const AddCart = () => {
   const location = useLocation();
-  const { saleId, storeId } = location.state as { saleId: number; storeId: number; };
+  const { saleId, storeId } = location.state as {
+    saleId: number;
+    storeId: number;
+  };
   const [quantity, setQuantity] = useState<number>(1);
   const [menu, setMenu] = useState<MenuItem>({
     saleId: 0,
@@ -27,15 +30,17 @@ const AddCart = () => {
   useEffect(() => {
     const fetchShopDetailGet = async () => {
       try {
-        const response = await MenuDetailGetService({ saleId: saleId.toString() });
-        setMenu(response)
-        console.log("메뉴 상세 요청 성공", response)
+        const response = await MenuDetailGetService({
+          saleId: saleId.toString(),
+        });
+        setMenu(response);
+        console.log("메뉴 상세 요청 성공", response);
       } catch (error) {
-        console.log("에러발생", error)
+        console.log("에러발생", error);
       }
-    }
+    };
     fetchShopDetailGet();
-  }, [])
+  }, []);
 
   return (
     <div>
@@ -50,16 +55,20 @@ const AddCart = () => {
       </div>
       {/* 음식 정보 */}
       <CartInfo menu={menu} />
-      <div className="center py-4 border-b-2">
+      <div className="py-4 border-b-2 center">
         <div className="w-[330px] border-2 border-green-400 rounded-3xl shadow-lg">
-          <ServiceBanner />
+          <BannerSlider />
         </div>
       </div>
       <hr className="border-4 border-gray-100" />
 
-      <div className="center py-2 border-b-2 ">
+      <div className="py-2 border-b-2 center ">
         <p className="font-extrabold mr-44">수량</p>
-        <FirstCountButton menu={menu} quantity={quantity} setQuantity={setQuantity} />
+        <FirstCountButton
+          menu={menu}
+          quantity={quantity}
+          setQuantity={setQuantity}
+        />
       </div>
       <hr className="border-4 border-gray-100 " />
       <div className="center">
