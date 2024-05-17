a4c4e0bc0d53175d085e5e4cab52fb49ad997ce3
Park Jong Hyuk <occultism22@naver.com>
2024-05-18 01:19:10 +0900

    [FE-Seller][Design] 가게 정보보기 페이지 UI 수정

--- frontend-seller/src/components/shop/ShopInfo.tsx
+++ frontend-seller/src/components/shop/ShopInfo.tsx
@@ -1,42 +1,23 @@
-import React from "react"
-import { getShopInfoType } from "../../types/shop/getShopInfoType"
+import React from "react";
+import { getShopInfoType } from "../../types/shop/getShopInfoType";
 
-import reviewImg from "../../assets/mypage-review.png"
-import updateInfoImg from "../../assets/mypage-change-info.png"
-import updateMenuImg from "../../assets/mypage-change-menu.png"
-import OpenShopAPI from "../../service/shop/OpenShopAPI"
-import TotalButton from "../ui/TotalButton"
-
-const ShopInfo:React.FC<getShopInfoType> = (props) => {
-    const { putFinishAll } = OpenShopAPI();
-    const handleFinishAll = () => {
-        putFinishAll();
-    };
-    
-    return (
-        <div className="flex flex-col items-center gap-3">
-            <TotalButton title="전체 판매 종료" onClick={() => handleFinishAll()} />
-            <img src={props.imageURL} alt="가게 대표 이미지" className="w-full h-[300px]"/>
-            <h1>가게명 : {props.name}</h1>
-            <h1>가게번호 : {props.tel}</h1>
-            <h1>가게주소 : {props.roadAddress}</h1>
-
-            <div className="flex flex-row justify-around w-full text-gray-500">
-                <div className="flex flex-col items-center">
-                    <img src={reviewImg} alt="리뷰 아이콘" className="w-[80px] h-[80px]" />
-                    <p>리뷰조회</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src={updateInfoImg} alt="리뷰 아이콘" className="w-[80px] h-[80px]" />
-                    <p>정보수정</p>
-                </div>
-                <div className="flex flex-col items-center">
-                    <img src={updateMenuImg} alt="리뷰 아이콘" className="w-[80px] h-[80px]" />
-                    <p>메뉴수정</p>
-                </div>
-            </div>
+const ShopInfo: React.FC<getShopInfoType> = (props) => {
+  return (
+    <div className="flex flex-col items-center gap-3">
+      <div className="flex flex-row gap-2">
+        <img
+          src={props.imageURL}
+          alt="가게 대표 이미지"
+          className="h-[300px] rounded-[20px]"
+        />
+        <div className="flex flex-col items-center border-2 rounded-[20px] gap-3 p-3">
+          <p>{props.name}</p>
+          <p>{props.tel}</p>
+          <p>{props.roadAddress}</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default ShopInfo;
