2884232ce642866ee2612154d42a836bd43e5dc2
Park Jong Hyuk <occultism22@naver.com>
2024-05-19 06:24:31 +0900

    [FE-Seller][Design] 메뉴판 디자인 수정

--- frontend-seller/src/components/menu/MenuList.tsx
+++ frontend-seller/src/components/menu/MenuList.tsx
@@ -5,96 +5,74 @@ import TotalButton from "../ui/TotalButton";
 import DeleteMenuAPI from "../../service/menu/DeleteMenuAPI";
 import RegisterSalesAPI from "../../service/sales/RegisterSalesAPI";
 import Cookies from "universal-cookie";
+import EditIcon from "../../assets/mypage-change-info.png";
+import DeleteIcon from "../../assets/trash-can-slash.png";
+import AddIcon from "../../assets/add-button.png";
 
 const MenuList: React.FC<MenuListType> = (props) => {
-  const cookies = new Cookies()
-  const storeId = cookies.get("storeId")
+  const cookies = new Cookies();
+  const storeId = cookies.get("storeId");
   const navigate = useNavigate();
   const { DeleteMenu } = DeleteMenuAPI();
   const { postSales } = RegisterSalesAPI();
-  // // 각 메뉴 아이템의 stock와 content 값을 관리하기 위한 상태 변수
-  // const [menuItems, setMenuItems] = useState(props.menusInfo.map(menu => ({
-  //   ...menu,
-  //   content: "",
-  //   stock: 0
-  // })));
-
-  // // 물품 재고 설정
-  // const handleChangeStock = (menuId: number, value: number) => {
-  //   setMenuItems(prevMenuItems =>
-  //     prevMenuItems.map(menu => {
-  //       if (menu.menuId === menuId) {
-  //         return {
-  //           ...menu,
-  //           stock: value
-  //         };
-  //       }
-  //       return menu;
-  //     })
-  //   );
-  // };
-
-  // // 물품 상세설명
-  // const handleChangeContent = (menuId: number, value: string) => {
-  //   setMenuItems(prevMenuItems =>
-  //     prevMenuItems.map(menu => {
-  //       if (menu.menuId === menuId) {
-  //         return {
-  //           ...menu,
-  //           content: value
-  //         };
-  //       }
-  //       return menu;
-  //     })
-  //   );
-  // };
 
   return (
-    <div className="flex flex-col items-center gap-4 relative w-full">
-      <button className="absolute right-4 top-2" onClick={() => navigate("/register-food")}>
-        메뉴 등록
+    <div className="flex flex-col items-center w-full gap-4">
+      <button
+        className="fixed right-4 bottom-[80px]"
+        onClick={() => navigate("/register-food")}
+      >
+        <img src={AddIcon} alt="메뉴 추가하기" className="w-[80px] h-[80px]" />
       </button>
 
-      <div className="flex flex-col items-center gap-4">
-        {props.menusInfo.map((menu) => (
-          <div
-            key={menu.menuId}
-            className="flex flex-col gap-3 border-2 rounded-lg"
-          >
-            <img src={menu.imageUrl} alt="" className="w-[40px] h-[40px]" />
-            <h1>메뉴이름 : {menu.name}</h1>
-            <h1>원가 : {menu.originalPrice}원</h1>
-            <h1>판매가 : {menu.sellPrice}원</h1>
-            <h1>할인율 : {menu.discountRate}%</h1>
-            {/* <input type="text" value={menu.content} onChange={(e) => handleChangeContent(menu.menuId, e.target.value)} placeholder="상세설명 설정"/>
-            <input type="number" value={menu.stock} onChange={(e) => handleChangeStock(menu.menuId, Number(e.target.value))} placeholder="재고 설정"/> */}
-            <TotalButton
-              title="메뉴 수정하기"
-              onClick={() => navigate(`/update/food/${menu.menuId}`)}
+      {props.menusInfo.map((menu) => (
+        <div
+          key={menu.menuId}
+          className="relative h-[300px] gap-3 border-2 rounded-3xl p-2 w-[680px] flex flex-col"
+        >
+          <div className="flex flex-row w-[600px] justify-between items-center gap-6">
+            <img
+              src={menu.imageUrl}
+              alt="메뉴 사진"
+              className="w-[200px] h-[200px] rounded-3xl"
             />
-            <TotalButton
-              title="메뉴 삭제하기"
-              onClick={() => DeleteMenu(menu.menuId)}
-            />
-            <TotalButton
-              title="판매 등록"
-              onClick={() =>
-                postSales({
-                  storeId: storeId,
-                  saleList: [
-                    {
-                      sellPrice: menu.sellPrice,
-                      content: "테스트 신청",
-                      stock: 10,
-                      menuId: menu.menuId,
-                    },
-                  ],
-                })
-              }
-            />
+            <div className="flex flex-col gap-3 w-full">
+              <p className="text-5xl font-bold">{menu.name}</p>
+              <h1>원가 : {menu.originalPrice}원</h1>
+              <h1>판매가 : {menu.sellPrice}원</h1>
+              <h1>할인율 : {menu.discountRate}%</h1>
+            </div>
           </div>
-        ))}
-      </div>
+          <img
+            src={EditIcon}
+            alt="메뉴 수정하기"
+            onClick={() => navigate(`/update/food/${menu.menuId}`)}
+            className="absolute w-[50px] h-[50px] right-[80px] top-4"
+          />
+          <img
+            src={DeleteIcon}
+            alt="메뉴 삭제하기"
+            onClick={() => DeleteMenu(menu.menuId)}
+            className="absolute w-[50px] h-[50px] right-[12px] top-4"
+          />
+          <TotalButton
+            title="판매 등록"
+            onClick={() =>
+              postSales({
+                storeId: storeId,
+                saleList: [
+                  {
+                    sellPrice: menu.sellPrice,
+                    content: "테스트 신청",
+                    stock: 10,
+                    menuId: menu.menuId,
+                  },
+                ],
+              })
+            }
+          />
+        </div>
+      ))}
     </div>
   );
 };
