c17c5c36e6e3f9eb4e05d2b933d463d4deed279e
jinyoung <kimyoung100@naver.com>
2024-05-19 02:42:00 +0900

    [FE-Consumer][Feat] 기부모드 API 연결 및 로직 수정

--- frontend-consumer/src/components/button/PaymentButton.tsx
+++ frontend-consumer/src/components/button/PaymentButton.tsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { CartInfo } from "../../types/CartType";
 import OrderPostForm from "../../services/orders/OrderPostService";
 import * as PortOne from "@portone/browser-sdk/v2";
+import { useNavigate } from "react-router-dom";
 
 const generateRandomId = (length: any) => {
   let result = "";
@@ -23,24 +24,36 @@ const PaymentButton = ({ cartInfo }: CheckPaymentProps) => {
     cartInfo.sellTotalPrice
   );
   const [orderInfoId, setOrderInfoId] = useState<number>(0);
-  const [consumerNickname, setCustomerNickName] = useState<string>("");
-  const [customerEmail, setCustomerEmail] = useState<string>("");
-  const [customerPhoneNumber, setCustomerPhoneNumber] = useState<string>("");
+  const [consumerNickname, setConsumerNickName] = useState<string>("");
+  const [consumerEmail, setConsumerEmail] = useState<string>("");
+  const [consumerPhoneNumber, setConsumerPhoneNumber] = useState<string>("");
+  const [isDonated, setIsDonated] = useState<boolean>(false);
+  const [paymentPrice, setPaymentPrice] = useState<number>(0);
 
   const handlePaymentClick = async () => {
     if (cartInfo.isOpened) {
       try {
         const response = await OrderPostForm();
         setOrderInfoId(response.orderInfoId);
-        setCustomerNickName(response.consumerNickname);
-        setCustomerEmail(response.customerEmail);
-        setCustomerPhoneNumber(response.customerPhoneNumber);
-        console.log(response)
+        setConsumerNickName(response.consumerNickname);
+        setConsumerEmail(response.customerEmail);
+        setConsumerPhoneNumber(response.customerPhoneNumber);
+        setIsDonated(response.isDonated);
+        setPaymentPrice(response.paymentPrice);
+        console.log("우하하");
+        console.log(response);
       } catch (error) {
         console.error(error);
       }
     }
   };
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    console.log(paymentPrice);
+    console.log(isDonated);
+  }, [paymentPrice, isDonated]);
 
   useEffect(() => {
     const processPayment = async () => {
@@ -50,40 +63,44 @@ const PaymentButton = ({ cartInfo }: CheckPaymentProps) => {
         const isConfirmed = confirm(
           "결제 후 마감 시간 전까지 매장 방문을 하지 않으시면 결제 금액은 사회에 환원됩니다. 결제를 진행하시겠습니까?"
         );
-  
+
         if (isConfirmed) {
-          // 사용자가 "확인"을 클릭한 경우, 결제 과정을 계속 진행합니다.
-          try {
-            const paymentId = `payment-${generateRandomId(30)}`;
-            const redirectUrl = `${window.location.origin}/consumer/payment-callback?orderInfoId=${orderInfoId}&paymentId=${paymentId}`;
-            const response = await PortOne.requestPayment({
-              storeId: import.meta.env.VITE_STORE_ID,
-              channelKey: import.meta.env.VITE_CHANNEL_KEY,
-              paymentId: paymentId,
-              orderName: "가은",
-              totalAmount: cartInfo.sellTotalPrice,
-              currency: "CURRENCY_KRW",
-              payMethod: "CARD",
-              redirectUrl: redirectUrl,
-              customer: {
-                fullName: consumerNickname,
-                email: customerEmail || "example@email.com",
-                phoneNumber: customerPhoneNumber || "010-1234-5678",
-              },
-            });
-            window.location.href = redirectUrl;
-            console.log(response);
-          } catch (error) {
-            console.error(error);
+          if (isDonated && paymentPrice === 0) {
+            navigate("/order-state", { state: { orderInfoId: orderInfoId } }); // 검증 성공 시 리다이렉트
+          } else {
+            // 사용자가 "확인"을 클릭한 경우, 결제 과정을 계속 진행합니다.
+            try {
+              const paymentId = `payment-${generateRandomId(30)}`;
+              const redirectUrl = `${window.location.origin}/consumer/payment-callback?orderInfoId=${orderInfoId}&paymentId=${paymentId}`;
+              const response = await PortOne.requestPayment({
+                storeId: import.meta.env.VITE_STORE_ID,
+                channelKey: import.meta.env.VITE_CHANNEL_KEY,
+                paymentId: paymentId,
+                orderName: "가은",
+                totalAmount: paymentPrice,
+                currency: "CURRENCY_KRW",
+                payMethod: "CARD",
+                redirectUrl: redirectUrl,
+                customer: {
+                  fullName: consumerNickname,
+                  email: consumerEmail || "example@email.com",
+                  phoneNumber: consumerPhoneNumber || "010-1234-5678",
+                },
+              });
+              window.location.href = redirectUrl;
+              console.log(response);
+            } catch (error) {
+              console.error(error);
+            }
           }
         } else {
           // 사용자가 "취소"를 클릭한 경우, 결제 과정을 취소합니다.
           console.log("결제가 취소되었습니다.");
-          alert("결제가 취소되었습니다.")
+          alert("결제가 취소되었습니다.");
         }
       }
     };
-  
+
     processPayment();
   }, [orderInfoId]);
   return (
