2d497fd5ac9a300877f47ba6fea9b02af088cb1b
Park Jong Hyuk <occultism22@naver.com>
2024-05-19 22:58:50 +0900

    [FE-Seller][Design] 알림 페이지 UI 추가 변경

--- frontend-seller/src/components/notification/NotificationList.tsx
+++ frontend-seller/src/components/notification/NotificationList.tsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { NotiListInfo } from "../../types/notification/NotificationListType";
 import { useNavigate } from "react-router-dom";
 import SellerNotiRead from "../../service/notification/SellerNotiRead";
 import letterIcon from "../../assets/letter-icon.png";
 import foodIcon from "../../assets/food-icon.png";
+import SellerNotiCount from "../../service/notification/SellerNotiCount";
 
 interface NotiProps {
   notiInfo: NotiListInfo[];
@@ -11,6 +12,20 @@ interface NotiProps {
 
 const NotificationList: React.FC<NotiProps> = (props) => {
   const navigate = useNavigate();
+  const [notiCount, setNotiCount] = useState<number>(0);
+
+  useEffect(() => {
+    const fetchNotiCount = async () => {
+      try {
+        const response = await SellerNotiCount();
+        setNotiCount(response.data.data.count);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchNotiCount();
+  }, []);
+
   const handleReadNoti = async (id: number, type: string, typeId?: number) => {
     try {
       await SellerNotiRead(id);
@@ -31,7 +46,12 @@ const NotificationList: React.FC<NotiProps> = (props) => {
   };
 
   return (
-    <div className="flex flex-col w-full gap-4">
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-row justify-center items-center w-full gap-4">
+        <p className="text-4xl">읽지 않은 편지가</p>
+        <p className="text-8xl">{notiCount}장</p>
+        <p className="text-4xl">있어요!</p>
+      </div>
       {props.notiInfo.map((item) => (
         <div
           key={item.id}
--- frontend-seller/src/components/ui/Header.tsx
+++ frontend-seller/src/components/ui/Header.tsx
@@ -1,27 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import GotoBack from "../../assets/back.png";
 import NotiIcon from "../../assets/notification.png";
-import SellerNotiCount from "../../service/notification/SellerNotiCount";
 
 const Header: React.FC = () => {
   const location = useLocation();
   const navigate = useNavigate();
-  const [notiCount, setNotiCount] = useState<number>(0);
-
-  useEffect(() => {
-    const fetchCountInfo = async () => {
-      try {
-        const response = await SellerNotiCount();
-        setNotiCount(response.data.data.count);
-        return response;
-      } catch (err) {
-        console.error(err);
-      }
-    };
-    fetchCountInfo();
-  }, []);
 
   // 주소에 따른 Header의 title변경
   let title = "";
@@ -65,7 +50,6 @@ const Header: React.FC = () => {
       </button>
       <h1>{title}</h1>
       <button onClick={() => navigate("/notification")}>
-        <p className="">{notiCount}</p>
         <img src={NotiIcon} alt="알림" className="w-[40px] h-[40px]" />
       </button>
     </header>
