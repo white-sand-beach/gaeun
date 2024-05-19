85657eb7a46b0a5179f4d2c080d38fb485043c91
jinyoung <kimyoung100@naver.com>
2024-05-19 16:39:17 +0900

    [FE-Seller][Design] 메뉴목록 css 바꾸기

--- frontend-seller/src/components/ui/TotalButton.tsx
+++ frontend-seller/src/components/ui/TotalButton.tsx
@@ -3,7 +3,7 @@ import { ButtonTypes } from "../../types/@common/ButtonTypes";
 const TotalButton: React.FC<ButtonTypes> = (props) => {
   return (
     <button
-      className={`common-btn rounded-xl text-xl h-[60px] ${props.title === "회원가입" ? "w-[508px]" : "w-[400px]"}`}
+      className={`common-btn rounded-xl text-[50px] h-[80px] ${props.title === "회원가입" ? "w-[508px]" : "w-[400px]"}`}
       onClick={props.onClick}
     >
       {props.title}
