ab9cfbfc242540bd491793244d72141cfee23c33
Anhyunsung <gustjd980112@gmail.com>
2024-05-19 18:30:51 +0900

    [FE-Seller][Design] Total 버튼 title = 판매 등록 버튼 수정

--- frontend-seller/src/components/ui/TotalButton.tsx
+++ frontend-seller/src/components/ui/TotalButton.tsx
@@ -3,7 +3,7 @@ import { ButtonTypes } from "../../types/@common/ButtonTypes";
 const TotalButton: React.FC<ButtonTypes> = (props) => {
   return (
     <button
-      className={`common-btn rounded-xl text-[50px] h-[80px]  ${props.title === "회원가입" && "w-[508px]" || props.title === "판매 등록" ? "w-[320px] rounded-[30px]" : "w-[400px]"}`}
+      className={`common-btn rounded-xl text-[50px] h-[80px]  ${props.title === "회원가입" && "w-[508px]" || props.title === "판매 등록" ? "w-[320px] rounded-[30px] text-[36px]" : "w-[400px]"}`}
       onClick={props.onClick}
     >
       {props.title}
